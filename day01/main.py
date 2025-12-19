cfcdb12dec08c29ec78e47488bd8876ff6a5733a
Collin Brittle <cnbrittle@gmail.com>
2025-12-19 12:33:11 -0500

    Fixes Day 1 pt 2

--- day01/main.py
+++ day01/main.py
@@ -7,16 +7,32 @@ def main():
             direction = line[0]
             rotation = int(line[1:])
 
+            # Hundreds of rotations, cut them out immediately.
+            rotations = rotation // 100
+            rotation = rotation - 100*rotations
+
             if direction.upper() == 'L':
                 rotation = -1 * rotation
 
-            position += rotation
+            new_position = position + rotation
+            # Check for negative rotation through zero.
+            if position > 0 and new_position < 0:
+                rotations +=1
+            # Check for landing on zero. Check this BEFORE the one below.
+            if new_position == 0:
+                rotations +=1
+            # Could land on up to 198, so check if we went over.
+            if new_position >= 100:
+                new_position -= 100
+                rotations +=1
 
-            rotations = position // 100
+            # This does the wrap from negative numbers back to positive.
+            new_position = new_position % 100
 
-            position += -100 * rotations
-            zeros += abs(rotations)
+            #print(f"pos{position}+{rotation}={new_position}: r{rotations} z{zeros}")
 
+            position = new_position
+            zeros += rotations
 
     print(zeros)
 
