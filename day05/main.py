ee6651c827868d9907051c981e9edd94a7f9fc57
Collin Brittle <cnbrittle@gmail.com>
2025-12-29 18:23:44 -0500

    Day 5 pt 2

--- day05/main.py
+++ day05/main.py
@@ -1,63 +1,64 @@
+from functools import total_ordering
+
+@total_ordering
 class FreshRange():
     def __init__(self, freshrange):
         lower, _ , higher = freshrange.partition('-')
         self.low = int(lower)
         self.high = int(higher)
 
-    def in_range(self, num):
-        if self.low <= num <= self.high:
-            return True
-        return False
-
-    def amend(self, other):
-        if self.low < other.low < self.high and self.low < other.low < self.high:
-            # Other's range is in self
-            return set()
-        elif other.low < self.low < other.high and other.low < self.high < other.high:
-            # Self's range is in other
-            return {other}
-        elif self.low > other.high or self.high < other.low:
-            # No overlap
-            return {other}
-        else:
-            # Overlap!
-            self.low = min(self.low, other.low)
-            self.high = max(self.high, other.high)
-            return set()
-
     def __repr__(self):
         return f"{self.low}-{self.high}"
 
+    def __eq__(self, other):
+        return self.low == other.low and self.high == other.high
+
+    def __lt__(self, other):
+        return self.low < other.low
+
+    def in_range(self, num):
+        return self.low <= num <= self.high
+
+    def length(self):
+        return self.high - self.low + 1
+
 def main():
-    # Setup
-    ranges = set()
+    # Setup ranges and ids.
+    ranges = []
     ids = set()
-    with open('example2.txt') as txt:
+    with open('input.txt') as txt:
         for line in txt:
             if line.find('-') != -1:
-                new_range = FreshRange(line)
-                print(f"Adding {new_range}")
-                new_ranges = set()
-
-                for r in ranges:
-                    new_ranges |= new_range.amend(r)
-                    print(new_ranges)
-                new_ranges.add(new_range)
-                print(new_ranges)
-
-                ranges = new_ranges
-
+                ranges.append(FreshRange(line))
             elif line != '\n':
                 ids.add(int(line))
 
+    # Flatten ranges
+    ranges.sort()
+    flattened = [ranges.pop(0)]
+    for r in ranges:
+        removals = []
+        for f in flattened:
+            # r.low >= f.low for all f, so if r.low < f.high, we have overlap.
+            if r.low <= f.high:
+                r.low = min(r.low, f.low)
+                r.high = max(r.high, f.high)
+                removals.append(f)
+        for i in removals:
+            flattened.remove(i)
+        flattened.append(r)
+        flattened.sort()
+
     # Compare ids in ranges
     fresh = set()
-    for r in ranges:
-        for i in ids:
+    for i in ids:
+        for r in flattened:
             if r.in_range(i):
                 fresh.add(i)
 
     print(len(fresh))
+
+    print(sum([r.length() for r in flattened]))
 
 if __name__ == "__main__":
     main()
