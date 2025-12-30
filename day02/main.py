0afd52030f027bf72b13430ee161cf70e972ff07
Collin Brittle <cnbrittle@gmail.com>
2025-12-29 20:36:45 -0500

    Day 2 pt 1 and 2

--- day02/main.py
+++ day02/main.py
@@ -1,21 +1,52 @@
 def main():
     total = 0
-    with open('input2.txt') as txt:
+    with open('input.txt') as txt:
         for line in txt:
             ranges = line.split(',')
             for r in ranges:
-                invalids = process_range(r)
+                #invalids = process_range(r)
+                invalids = process_relaxed_range(r)
                 total += sum(invalids)
 
     print(total)
 
 def process_range(id_range):
+    invalids = []
     lower, upper = id_range.split('-', 1)
 
-    if len(lower) % 2 == 0:
-        pass
+    for i in range(int(lower), int(upper) + 1):
+        str_i = str(i)
+        if len(str_i) % 2 == 0:
+            mid = len(str_i) // 2
+            if str_i[:mid] == str_i[mid:]:
+                invalids.append(i)
 
-    return [int(lower)]
+    #print(id_range, invalids)
+    return invalids
+
+def process_relaxed_range(id_range):
+    invalids = []
+    lower, upper = id_range.split('-', 1)
+
+    for i in range(int(lower), int(upper) + 1):
+        str_i = str(i)
+        len_i = len(str_i)
+
+        factors = [1]
+        for f in range(2, len_i // 2 + 1):
+            if len_i % f == 0:
+                factors.append(f)
+
+        substrings = []
+        for f in factors:
+            substrings.append(str_i[:f]*(len_i // f))
+        #print(i, factors, substrings)
+
+        if str_i in substrings and len_i > 1:
+            invalids.append(i)
+
+    #print(id_range, invalids)
+    return invalids
 
 if __name__ == '__main__':
     main()
