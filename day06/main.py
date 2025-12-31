9c1bd7ff7668811c1a781f98565ce8219698080a
Collin Brittle <cnbrittle@gmail.com>
2025-12-31 14:29:06 -0500

    Day 6 pt 2

--- day06/main.py
+++ day06/main.py
@@ -1,6 +1,6 @@
 from math import prod
 
-def main():
+def pt1():
     with open('input.txt') as txt:
         lines = []
         for line in txt:
@@ -23,5 +23,48 @@ def main():
 
     print(total)
 
+class Prob:
+    __slots__ = ('op', 'nums')
+
+    def __init__(self, operator, number=None):
+        self.op = operator
+        self.nums = []
+        if number is not None:
+            self.nums.append(number)
+
+    def solve(self):
+        if self.op == '+':
+            return sum(self.nums)
+        elif self.op == '*':
+            return prod(self.nums)
+
+def pt2():
+    with open('input.txt') as txt:
+        lines = []
+        for line in txt:
+            lines.append(list(line))
+
+    # Transpose lines into columns.
+    lines = zip(*lines)
+
+    total = 0
+    problem = None
+    for line in lines:
+        # Always a 'blank' line between problems (newlines at the end).
+        # Work on one problem at a time, then solve and add the total at the end.
+        if len(''.join(line).strip()) == 0:
+            total += problem.solve()
+        # Always an operator at the end of the line for new problems.
+        elif line[-1] == "*" or line[-1] == "+":
+            op = line[-1]
+            num = int(''.join(line[:-1]).strip())
+            problem = Prob(op, num)
+        # Everything else is a number.
+        else:
+            num = int(''.join(line).strip())
+            problem.nums.append(num)
+
+    print(total)
+
 if __name__ == "__main__":
-    main()
+    pt2()
